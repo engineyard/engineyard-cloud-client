98285b828e0fd7a7bfd4756046d5e56f32c9cdbd
Martin Emde <martin.emde@gmail.com>
2012-05-22 09:44:23 -0700

    Next version will be first real live version

--- lib/engineyard-cloud-client/version.rb
+++ lib/engineyard-cloud-client/version.rb
@@ -1,7 +1,7 @@
 # This file is maintained by a herd of rabid monkeys with Rakes.
 module EY
   class CloudClient
-    VERSION = '0.1.5.pre'
+    VERSION = '1.0.0.pre'
   end
 end
 # Please be aware that the monkeys like tho throw poo sometimes.
