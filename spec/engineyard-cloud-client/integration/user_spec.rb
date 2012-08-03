86073568a8416d9d0d17ff3e6e9cdb6b4702d544
Martin Emde <martin.emde@gmail.com>
2012-08-03 13:52:29 -0700

    Minor spec cleanup

--- spec/engineyard-cloud-client/integration/user_spec.rb
+++ spec/engineyard-cloud-client/integration/user_spec.rb
@@ -5,13 +5,11 @@ describe EY::CloudClient::User do
     FakeWeb.allow_net_connect = true
   end
 
-  describe ".all" do
-    it "returns all accounts" do
-      api = scenario_cloud_client "User Name"
-      user = api.current_user
-      user.name.should == 'User Name'
-      user.accounts.size.should == 1
-      user.accounts.first.name.should == 'main'
-    end
+  it "loads current user and returns all accounts" do
+    api = scenario_cloud_client "User Name"
+    user = api.current_user
+    user.name.should == 'User Name'
+    user.accounts.size.should == 1
+    user.accounts.first.name.should == 'main'
   end
 end
