31cdfacd2ab4c867f568bbec407d731137d0bae9
Martin Emde <martin.emde@gmail.com>
2012-08-14 10:14:28 -0700

    Add .pre for next release

--- lib/engineyard-cloud-client/version.rb
+++ lib/engineyard-cloud-client/version.rb
@@ -1,7 +1,7 @@
 # This file is maintained by a herd of rabid monkeys with Rakes.
 module EY
   class CloudClient
-    VERSION = '1.0.4'
+    VERSION = '1.0.5.pre'
   end
 end
 # Please be aware that the monkeys like tho throw poo sometimes.
