0b660fe6724ab111f51501b1efb5059ce27a62e2
Martin Emde <martin.emde@gmail.com>
2012-10-25 11:58:14 -0700

    Bump versions for release 1.0.7

--- lib/engineyard-cloud-client/version.rb
+++ lib/engineyard-cloud-client/version.rb
@@ -1,7 +1,7 @@
 # This file is maintained by a herd of rabid monkeys with Rakes.
 module EY
   class CloudClient
-    VERSION = '1.0.7.pre'
+    VERSION = '1.0.7'
   end
 end
 # Please be aware that the monkeys like tho throw poo sometimes.
