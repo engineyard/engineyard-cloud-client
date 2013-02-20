4e6c05820890d85b9267832b7110501b41b208b3
Martin Emde <martin.emde@gmail.com>
2013-02-20 11:51:51 -0800

    Remove more spec run noise

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,7 +36,6 @@ support.each{|helper| require helper }
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run :focus
 
   config.include SpecHelpers
 
