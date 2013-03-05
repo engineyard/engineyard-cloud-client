619c5f742bc4c5abe6474e7ac1a521d6de35acb1
Martin Emde <martin.emde@gmail.com>
2013-03-04 17:14:27 -0800

    Test some of the improved error handling cases

--- spec/engineyard-cloud-client/api_spec.rb
+++ spec/engineyard-cloud-client/api_spec.rb
@@ -38,4 +38,28 @@ describe EY::CloudClient do
       EY::CloudClient.new.authenticate!("a@b.com", "foo")
     }.should raise_error(EY::CloudClient::RequestFailed, /API is temporarily unavailable/)
   end
+
+  it "raises RequestFailed with a friendly error when the response contains a message in json" do
+    FakeWeb.register_uri(:post, "https://cloud.engineyard.com/api/v2/authenticate", :status => 409, :content_type => 'application/json', :body => %|{"message":"Important information regarding your failure"}|)
+
+    lambda {
+      EY::CloudClient.new.authenticate!("a@b.com", "foo")
+    }.should raise_error(EY::CloudClient::RequestFailed, /Error: Important information regarding your failure/)
+  end
+
+  it "raises RequestFailed with a semi-useful error message when the body is empty" do
+    FakeWeb.register_uri(:post, "https://cloud.engineyard.com/api/v2/authenticate", :status => 409, :content_type => 'text/plain', :body => "")
+
+    lambda {
+      EY::CloudClient.new.authenticate!("a@b.com", "foo")
+    }.should raise_error(EY::CloudClient::RequestFailed, /Error: 409 Conflict/)
+  end
+
+  it "raises RequestFailed with the response body when the content type is not json" do
+    FakeWeb.register_uri(:post, "https://cloud.engineyard.com/api/v2/authenticate", :status => 409, :content_type => 'text/plain', :body => "What a useful error message!")
+
+    lambda {
+      EY::CloudClient.new.authenticate!("a@b.com", "foo")
+    }.should raise_error(EY::CloudClient::RequestFailed, /Error: 409 Conflict What a useful error message!/)
+  end
 end
