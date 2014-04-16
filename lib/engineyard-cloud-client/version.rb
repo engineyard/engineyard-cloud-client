fa5e062e2d779e0540396d3514e4ac5ecc863723
Martin Emde <me@martinemde.com>
2014-04-16 09:34:58 -0700

    Next release will be 2.1.0, which includes backwards compatible new features.

--- lib/engineyard-cloud-client/version.rb
+++ lib/engineyard-cloud-client/version.rb
@@ -1,7 +1,7 @@
 # This file is maintained by a herd of rabid monkeys with Rakes.
 module EY
   class CloudClient
-    VERSION = '2.0.2-pre.1'
+    VERSION = '2.1.0-pre.1'
   end
 end
-# Please be aware that the monkeys like tho throw poo sometimes.
+# Please be aware that the monkeys like to throw poo sometimes.
