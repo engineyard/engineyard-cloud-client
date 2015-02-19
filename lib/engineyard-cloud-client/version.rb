927ace79f44e961298bd336069b30088526e1e4c
Jim Neath <jimneath@gmail.com>
2015-02-19 09:28:04 +0000

    bump version

--- lib/engineyard-cloud-client/version.rb
+++ lib/engineyard-cloud-client/version.rb
@@ -1,7 +1,7 @@
 # This file is maintained by a herd of rabid monkeys with Rakes.
 module EY
   class CloudClient
-    VERSION = '2.1.2-pre.1'
+    VERSION = '2.1.2'
   end
 end
 # Please be aware that the monkeys like tho throw poo sometimes.
