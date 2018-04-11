25332856807e003a795c35e2642cd99ba8687367
Igor Barbosa <igorbarbosa.p@gmail.com>
2018-04-11 16:20:35 -0300

    Bump version to 3.0.0

--- lib/engineyard-cloud-client/version.rb
+++ lib/engineyard-cloud-client/version.rb
@@ -1,7 +1,7 @@
 # This file is maintained by a herd of rabid monkeys with Rakes.
 module EY
   class CloudClient
-    VERSION = '2.1.2'
+    VERSION = '3.0.0'
   end
 end
 # Please be aware that the monkeys like tho throw poo sometimes.
